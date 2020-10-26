ac948c23b392b8b3548eaa053802f42b21aac160
Polaasia <51904848+Polaasia@users.noreply.github.com>
2020-10-26 14:38:21 +0100

    Added initial project

---
+++ tutorial_01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace tutorial_01
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+            var url = @"http://www.pja.edu.pl/dziekanat";
+
+            using (var httpClient = new HttpClient())
+            {
+
+
+                using (var response = await httpClient.GetAsync(url))
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+
+
+                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                    var matches = regex.Matches(content);
+
+                    foreach (var match in matches) //foreach tab tab
+                    {
+                        Console.WriteLine(match.ToString());
+                    }
+
+                }
+            }
+        }
+         
+    }
+}
