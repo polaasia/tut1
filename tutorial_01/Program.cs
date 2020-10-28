7da70369ad2a9eeb737e78ace19e660c45f31d9d
Polaasia <51904848+Polaasia@users.noreply.github.com>
2020-10-28 16:45:20 +0100

    Task 2

--- tutorial_01/Program.cs
+++ tutorial_01/Program.cs
@@ -9,27 +9,25 @@ namespace tutorial_01
     {
         public static async Task Main(string[] args)
         {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(args[0]);
 
-            using (var httpClient = new HttpClient())
+
+            if(response.IsSuccessStatusCode)
             {
+                var content = await response.Content.ReadAsStringAsync();
 
-                using (var response = await httpClient.GetAsync(args[0]))
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(content);
+
+                foreach (var match in matches)
                 {
-                    var content = await response.Content.ReadAsStringAsync();
-
-
-                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
-
-                    var matches = regex.Matches(content);
-
-                    foreach (var match in matches) 
-                    {
-                        Console.WriteLine(match.ToString());
-                    }
-
+                    Console.WriteLine(match.ToString());
                 }
             }
+
         }
-         
     }
 }
+         
